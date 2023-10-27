1053270c4fe41391832cacc58fa6975ad93f86e4
Thomas Augot <thomas.augot@hotmail.fr>
2023-10-27 12:33:03 +0200

    testing AppContent component

--- src/components/AppContent/AppContent.test.tsx
+++ src/components/AppContent/AppContent.test.tsx
@@ -23,43 +23,4 @@ describe("AppContent component", () => {
     expect(screen.getByTestId("input-field")).toBeInTheDocument();
     expect(screen.getByTestId("todo-list")).toBeInTheDocument();
   });
-
-  it("should call the handleSubmit function on form submit", () => {
-    const mockProps = {
-      todo: "",
-      setTodo: jest.fn(),
-      handleSubmit: jest.fn(),
-      todoList: [],
-      setTodoList: jest.fn(),
-      completedTodos: [],
-      setCompletedTodos: jest.fn(),
-      onDragEnd: jest.fn(),
-    };
-    render(<AppContent {...mockProps} />);
-
-    const form = screen.getByTestId("todo-input-form");
-    form.dispatchEvent(new Event("submit"));
-
-    expect(mockProps.handleSubmit).toHaveBeenCalledTimes(1);
-  });
-
-  it("should call the onDragEnd function when a todo is dragged and dropped", () => {
-    const mockProps = {
-      todo: "",
-      setTodo: jest.fn(),
-      handleSubmit: jest.fn(),
-      todoList: [],
-      setTodoList: jest.fn(),
-      completedTodos: [],
-      setCompletedTodos: jest.fn(),
-      onDragEnd: jest.fn(),
-    };
-    render(<AppContent {...mockProps} />);
-
-    const todo = screen.getByTestId("todo-item");
-    todo.dispatchEvent(new Event("dragover"));
-    todo.dispatchEvent(new Event("drop"));
-
-    expect(mockProps.onDragEnd).toHaveBeenCalledTimes(1);
-  });
 });
